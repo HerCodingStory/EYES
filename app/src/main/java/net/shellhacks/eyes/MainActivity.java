d638c24c8ecd56296a1f2075a7f61118a7a9b0ef
Amanda Fernandez <afern619@fiu.edu>
2018-09-15 04:44:38 -0400

    Audio

--- app/src/main/java/net/shellhacks/eyes/MainActivity.java
+++ app/src/main/java/net/shellhacks/eyes/MainActivity.java
@@ -1,13 +1,36 @@
 package net.shellhacks.eyes;
-
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
 
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+ MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1=findViewById(R.id.button5);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player=MediaPlayer.create(MainActivity.this,R.raw.tutorial);
+                player.start();
+            }
+        });
+
+        button1.setOnLongClickListener(new View.OnLongClickListener(){
+            public boolean onLongClick(View v){
+                player.pause();
+                return true;
+            }
+        });
+
     }
 }
